4bfc025db25fefc21ffd59187c71fd60f88e0f43
lofei <remilia586334@hotmail.com>
2019-05-04 18:09:36 +0800

    添加环境变量

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 \cp -rf ./createReactComponent ~/
-echo "安装完成. 请使用: createReactComponent.sh 组件名"
+cd ~/
+HOME=`echo $(pwd)`
+echo "" >> ~/.bash_profile
+echo "# createReactComponent" >> ~/.bash_profile
+echo "alias crc=${HOME}/createReactComponent/createReactComponent.sh" >> ~/.bash_profile
+echo "" >> ~/.bash_profile
+
+# 如果使用是zsh或者其他shell请手动更新配置文件
+source ~/.bash_profile
+
+echo "安装完成. 创建react组件方法: crc 组件名"
